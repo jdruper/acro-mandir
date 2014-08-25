745b948c6d6f3f6d202dfbe27a10996cbb041ef9
Jose Beita <jdruper@gmail.com>
2014-08-25 10:23:53 -0500

    commit inicial

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+// $('.color-principiante').on('click', function(){
+// 	$('.div-ambos').addClass('hide');
+// 	$('.div-principiante').parent().removeClass('hide');
+// });
+
+// $('.color-avanzado').on('click', function(){
+// 	$('.div-ambos').addClass('hide');
+// 	$('.div-avanzado').parent().removeClass('hide');
+// });
+
+$('.div-ambos').nivel();
